8e0f9a6c3315e890af7d00eec548f4fac9fce454
Joseph <56702275+joeguy57@users.noreply.github.com>
2021-04-20 22:28:16 -0600

    Finished basic layout for my website. Fully functional and has full css.
    Things to do:
    Create Carousel for reference
    Contact references
    Input all text replacing lorem ipsum

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+const portfolioLinks = document.querySelectorAll('.portfolio__item');
+navToggle.addEventListener('click',()=>{
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=>{
+        document.body.classList.remove('nav-open');
+    });
+});
+
+portfolioLinks.forEach(portLink => {
+    portLink.addEventListener('click', ()=>{
+        var srcLink = portLink.querySelector('img').src;
+        localStorage.setItem('imageNeeded',srcLink)
+    })
+})
---
+++ JS/portfolioItem.js
@@ -0,0 +1,40 @@
+const getImage = localStorage.getItem('imageNeeded');
+const introImage = document.querySelector('.intro__img');
+const portImage = document.querySelector('.portfolio-item-individual__img')
+
+//Set up texts
+const portfolioStrong = document.querySelector('.section__title--intro strong');
+
+//Here We will use srcTag[2] in order to make our comparison.
+let srcTag = getImage.split("Portfolio/");
+let projectText = "";
+let title = "";
+switch (srcTag[2]){
+    case "port__1.jpg":
+        title = "";
+        projectText = "Accessibilities Tool";
+        break;
+    case "port__2.jpg":
+        title = "";
+        projectText = "Accessibilities Tool";
+        break;
+    case "port__3.jpg":
+        title = "";
+        projectText = "Accessibilities Tool";
+        break;
+    case "port__4.jpg":
+        title = "";
+        projectText = "Accessibilities Tool";
+        break;
+    default:
+        title = "Project Does not Exist";
+        projectText = "No Image was Found";
+        break;
+}
+
+introImage.src = getImage;
+introImage.alt = "This is the main Photo"
+portImage.src = getImage;
+portImage.alt = 'This is the Portfolio Image'
+
+portfolioStrong.innerHTML = projectText;
