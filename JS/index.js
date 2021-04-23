2fdbc3f17b5f0888957bcf85bb91adc4cf66e5d1
Joseph <56702275+joeguy57@users.noreply.github.com>
2021-04-23 01:15:26 -0600

    Created a branch for testing. and a branch to continue work.

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,26 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
-const portfolioLinks = document.querySelectorAll('.portfolio__item');
+const portfolioLinks = document.querySelectorAll('.portfolio__item img');
+
+function noScroll() {
+    window.scrollTo(0,0);
+}
+
 navToggle.addEventListener('click',()=>{
     document.body.classList.toggle('nav-open');
+    window.addEventListener('scroll',noScroll);
 });
 
 navLinks.forEach(link => {
     link.addEventListener('click', ()=>{
         document.body.classList.remove('nav-open');
+        window.removeEventListener('scroll', noScroll)
     });
 });
 
 portfolioLinks.forEach(portLink => {
     portLink.addEventListener('click', ()=>{
-        let srcLink = portLink.querySelector('img').src;
+        let srcLink = portLink.src;
         localStorage.setItem('imageNeeded',srcLink)
     })
 })
