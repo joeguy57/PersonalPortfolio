2a68ece6f48fa0c2c84045bef2c3c51846f0128b
Joseph <56702275+joeguy57@users.noreply.github.com>
2022-03-31 16:59:16 -0600

    Editted hamburger menu and overlay to accompany the transitions.

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,37 @@
-const navToggle = document.querySelector('.nav-toggle');
+const navToggle = document.querySelector('.ham');
+const menu = document.querySelector('.menu')
 const navLinks = document.querySelectorAll('.nav__link');
 const portfolioLinks = document.querySelectorAll('.portfolio__item img');
 
-function noScroll() {
+function noscroll() {
     window.scrollTo(0,0);
 }
 
-navToggle.addEventListener('click',()=>{
-    document.body.classList.toggle('nav-open');
-    window.addEventListener('scroll',noScroll);
-});
+navToggle.addEventListener('click',openNav);
+
+function openNav(){
+    if (document.getElementById("myNav").style.width === ""){
+        document.getElementById("myNav").style.width = "100%";
+        window.addEventListener('scroll',noscroll);
+    }
+    else {
+        document.getElementById("myNav").style.width = "";
+        window.removeEventListener('scroll', noscroll);
+    }
+}
+
+
 
 navLinks.forEach(link => {
-    link.addEventListener('click', ()=>{
+    link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-        window.removeEventListener('scroll', noScroll)
+        enableScroll()
     });
 });
 
 portfolioLinks.forEach(portLink => {
-    portLink.addEventListener('click', ()=>{
+    portLink.addEventListener('click', () => {
         let srcLink = portLink.src;
-        localStorage.setItem('imageNeeded',srcLink)
+        localStorage.setItem('imageNeeded', srcLink)
     })
 })
