00644e03585a9288718e893752ff8c00868c7e35
Joseph <56702275+joeguy57@users.noreply.github.com>
2023-02-12 15:43:52 -0600

    Added WorkExperience

--- JS/index.js
+++ JS/index.js
@@ -1,37 +1,40 @@
-const navToggle = document.querySelector('.ham');
-const menu = document.querySelector('.menu')
-const navLinks = document.querySelectorAll('.nav__link');
-const portfolioLinks = document.querySelectorAll('.portfolio__item img');
+const navToggle = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelectorAll(".nav__link");
+const hamMenu = document.querySelector('.hamMenu')
+const portfolioLinks = document.querySelectorAll(".portfolio__item img");
 
 function noscroll() {
-    window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 }
 
-navToggle.addEventListener('click',openNav);
+navToggle.addEventListener("click", openNav);
 
-function openNav(){
-    if (document.getElementById("myNav").style.width === ""){
-        document.getElementById("myNav").style.width = "100%";
-        window.addEventListener('scroll',noscroll);
-    }
-    else {
-        document.getElementById("myNav").style.width = "";
-        window.removeEventListener('scroll', noscroll);
-    }
+function openNav() {
+  if (document.getElementById("myNav").style.width === "") {
+    document.getElementById("myNav").style.width = "100%";
+    hamMenu.classList.add("fixed-menu");
+  } else {
+    document.getElementById("myNav").style.width = "";
+    hamMenu.classList.remove("fixed-menu");
+  }
 }
 
-
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-        enableScroll()
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const sectionId = e.target.getAttribute("href");
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+    document.getElementById("myNav").style.width = "";
+    hamMenu.classList.remove("fixed-menu");
+    hamMenu.classList.toggle('active');
+  });
 });
 
-portfolioLinks.forEach(portLink => {
-    portLink.addEventListener('click', () => {
-        let srcLink = portLink.src;
-        localStorage.setItem('imageNeeded', srcLink)
-    })
-})
+portfolioLinks.forEach((portLink) => {
+  portLink.addEventListener("click", () => {
+    let srcLink = portLink.src;
+    localStorage.setItem("imageNeeded", srcLink);
+  });
+});
