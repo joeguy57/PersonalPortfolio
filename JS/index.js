b5191b832448297aa349edb3c02940c65703d4d5
Joseph <56702275+joeguy57@users.noreply.github.com>
2023-02-16 15:03:42 -0600

    adjsted time

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,19 @@
 const navToggle = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
 const navLinks = document.querySelectorAll(".nav__link");
-const hamMenu = document.querySelector('.hamMenu')
+const hamMenu = document.querySelector(".hamMenu");
 const portfolioLinks = document.querySelectorAll(".portfolio__item img");
 // Loader
 const loader = document.querySelector(".loader");
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-window.addEventListener('load', async function () {
-  await delay(2500);
-  body.style.overflow = 'scroll';
-  loader.style.display = 'none';
-})
-
-loading;
+window.addEventListener("load", async () => {
+  await delay(5000);
+  body.style.overflow = "scroll";
+  loader.style.display = "none";
+});
 
 function noscroll() {
   window.scrollTo(0, 0);
@@ -44,12 +42,10 @@ navLinks.forEach((link) => {
       hamMenu.classList.remove("fixed-menu");
       hamMenu.classList.toggle("active");
     } catch (error) {
-      let link = '/' + sectionId;
-      link = link.replace('.html/', '.html');
-      window.open(link,"_self");
+      let link = "/" + sectionId;
+      link = link.replace(".html/", ".html");
+      window.open(link, "_self");
     }
-    
-    
   });
 });
 
